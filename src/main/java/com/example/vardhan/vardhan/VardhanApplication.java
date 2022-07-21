1100a175fd840e826ab7d7da1c9898523ea813ff
svsraghu <svsrvardhan@gmail.com>
2022-07-21 21:37:39 +0530

    first commit

---
+++ src/main/java/com/example/vardhan/vardhan/VardhanApplication.java
@@ -0,0 +1,13 @@
+package com.example.vardhan.vardhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VardhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VardhanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vardhan/vardhan/VardhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vardhan.vardhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VardhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
